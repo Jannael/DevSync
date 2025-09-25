8619bc6d34c9b2314957bda30c3b0942b0e4bcd5
Jannael Orlando <orlandojannael@gmail.com>
2025-09-25 09:39:03 -0600

    group-mongoose schema

--- src/backend/database/schemas/node/group.ts
+++ src/backend/database/schemas/node/group.ts
@@ -3,13 +3,22 @@ import config from './../../../config/config'
 const { Schema } = mongoose
 
 const techLeadSchema = new Schema({
-  fullName: { type: String },
-  _id: { type: Schema.Types.ObjectId }
+  fullName: { type: String, require: true },
+  _id: { type: Schema.Types.ObjectId, require: true }
+}, {
+  ...config.database.mongodb.schemaOptions,
+  _id: false
 })
 
 const schema = new Schema({
   _id: { type: Schema.Types.ObjectId },
-  techLead: techLeadSchema
+  techLead: { type: techLeadSchema, require: true },
+  name: { type: String, require: true },
+  repository: { type: String },
+  member: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'user'
+  }]
 }, {
   ...config.database.mongodb.schemaOptions,
   collection: 'group'

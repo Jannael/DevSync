4159d0f5e660db1b855f2580d2fdc8ac21cc7f1b
Jannael Orlando <orlandojannael@gmail.com>
2025-09-28 16:26:37 -0600

    hashing accounts for db

--- src/backend/model/user/model.ts
+++ src/backend/model/user/model.ts
@@ -4,16 +4,18 @@ import dbModel from './../../database/schemas/node/user'
 import { IUser } from './../../interface/user'
 import bcrypt from 'bcrypt'
 import dotenv from 'dotenv'
+import jwt from 'jsonwebtoken'
 
 dotenv.config({ quiet: true })
-const { BCRYPT_SALT_HASH } = process.env as Pick<IEnv, 'BCRYPT_SALT_HASH'>
+const { BCRYPT_SALT_HASH, JWT_DATABASE_ENV } = process.env as Pick<IEnv, 'BCRYPT_SALT_HASH' | 'JWT_DATABASE_ENV'>
 
 const model = {
   create: {
     user: async function (data: IUser): Promise<IUser> {
       try {
         const hashedPwd = await bcrypt.hash(data.pwd, BCRYPT_SALT_HASH)
-        const payload = { ...data, pwd: hashedPwd }
+        const hashAccount = jwt.sign(data.account, JWT_DATABASE_ENV)
+        const payload = { ...data, pwd: hashedPwd, account: hashAccount }
         const result = await dbModel.insertOne({ ...payload })
         return result as IUser
       } catch (e: any) {

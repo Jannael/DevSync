3a873f39305930eee1476e8c47b90912b107306c
Jannael Orlando <orlandojannael@gmail.com>
2025-09-28 13:49:12 -0600

    hashing pwds

--- src/backend/model/user/model.ts
+++ src/backend/model/user/model.ts
@@ -1,12 +1,19 @@
 import { DatabaseError, DuplicateData } from '../../error/error'
 import dbModel from './../../database/schemas/node/user'
 import { IUser } from './../../interface/user'
+import bcrypt from 'bcrypt'
+import dotenv from 'dotenv'
+
+dotenv.config({ quiet: true })
+const { BCRYPT_SALT_HASH } = process.env as { BCRYPT_SALT_HASH: string }
 
 const model = {
   create: {
     user: async function (data: IUser): Promise<IUser> {
       try {
-        const result = await dbModel.insertOne({ ...data })
+        const hashedPwd = await bcrypt.hash(data.pwd, BCRYPT_SALT_HASH)
+        const payload = { ...data, pwd: hashedPwd }
+        const result = await dbModel.insertOne({ ...payload })
         return result as IUser
       } catch (e: any) {
         if (e?.code === 11000) {

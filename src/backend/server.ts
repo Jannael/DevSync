0663ebecd65b239cc175990ae3a60cc441d7ae3c
Jannael Orlando <orlandojannael@gmail.com>
2025-10-25 17:41:19 -0600

    entry point

---
+++ src/backend/server.ts
@@ -0,0 +1,14 @@
+import { createApp } from './app'
+import dotenv from 'dotenv'
+
+dotenv.config({ quiet: true })
+
+const PORT = process.env.PORT as string
+
+async function init (PORT: string): Promise<void> {
+  const app = await createApp()
+  app.listen(PORT)
+}
+
+init(PORT)
+  .catch(e => console.error('something went wrong : ', e))

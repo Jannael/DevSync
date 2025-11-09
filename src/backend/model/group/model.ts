8151a8855e30f6e1dd1f69aa8eff69f1235af1ac
Jannael Orlando <orlandojannael@gmail.com>
2025-11-09 08:45:09 -0600

    delete group fn

--- src/backend/model/group/model.ts
+++ src/backend/model/group/model.ts
@@ -3,6 +3,7 @@ import dbModel from './../../database/schemas/node/group'
 import { IGroup } from '../../interface/group'
 import validator from '../../validator/validator'
 import { DatabaseError, Forbidden, NotFound, UserBadRequest } from '../../error/error'
+import UserModel from '../user/model'
 
 const model = {
   get: async function (id: Types.ObjectId): Promise<IGroup> {
@@ -43,6 +44,27 @@ const model = {
       else if (e instanceof Forbidden) throw e
       else if (e instanceof NotFound) throw e
       throw new DatabaseError('Failed to save', 'The group was not updated, something went wrong please try again')
+    }
+  },
+  remove: async function (techLeadId: Types.ObjectId, groupId: Types.ObjectId): Promise<boolean> {
+    try {
+      const isTechLead = await dbModel.exists({ _id: groupId, 'techLead._id': techLeadId })
+      if (isTechLead === null) throw new Forbidden('Access denied', 'Only tech leads can delete a group')
+
+      const members = await dbModel.findOne({ _id: groupId }, { member: 1 }).lean()
+      if (members === null) throw new NotFound('Group not found', 'The group you are trying to delete does not exist')
+      for (const member of members.member) {
+        await UserModel.group.remove(member._id, groupId)
+      }
+
+      const deleted = await dbModel.deleteOne({ _id: groupId })
+      if (deleted.deletedCount === 0) throw new NotFound('Group not found', 'The group you are trying to delete does not exist')
+      return true
+    } catch (e) {
+      if (e instanceof Forbidden) throw e
+      else if (e instanceof NotFound) throw e
+      else if (e instanceof UserBadRequest) throw e
+      throw new DatabaseError('Failed to remove', 'The group was not deleted, something went wrong please try again')
     }
   }
 }

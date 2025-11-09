27313eadba8affa156bb42b4d37c92112c06b051
Jannael Orlando <orlandojannael@gmail.com>
2025-11-09 08:35:16 -0600

    get group model fn improved

--- src/backend/error/error.ts
+++ src/backend/error/error.ts
@@ -30,7 +30,7 @@ export function createError<T extends string> (
   }
 }
 
-type IDatabaseError = 'Connection error' | 'Failed to save' | 'Failed to remove'
+type IDatabaseError = 'Connection error' | 'Failed to save' | 'Failed to remove' | 'Failed to access data'
 export const DatabaseError = createError<IDatabaseError>(500, 'databaseError')
 
 type IUserBadRequest = 'Missing data' | 'Invalid credentials'
@@ -39,11 +39,11 @@ export const UserBadRequest = createError<IUserBadRequest>(400, 'userBadRequest'
 type IDuplicateData = 'User already exists'
 export const DuplicateData = createError<IDuplicateData>(409, 'duplicateData')
 
-type INotFound = 'User not found'
+type INotFound = 'User not found' | 'Group not found'
 export const NotFound = createError<INotFound>(404, 'NotFound')
 
 type IServerError = 'Operation Failed'
 export const ServerError = createError<IServerError>(500, 'ServerError')
 
-type IForbidden = 'Invalid account'
+type IForbidden = 'Invalid account' | 'Access denied'
 export const Forbidden = createError<IForbidden>(403, 'Forbidden')
--- src/backend/model/group/model.ts
+++ src/backend/model/group/model.ts
@@ -1,14 +1,37 @@
 import { Types } from 'mongoose'
 import dbModel from './../../database/schemas/node/group'
 import { IGroup } from '../../interface/group'
+import validator from '../../validator/validator'
+import { DatabaseError, Forbidden, NotFound } from '../../error/error'
 
 const model = {
-  get: async function (id: Types.ObjectId): Promise<IGroup | null> {
-    return await dbModel.findOne({ _id: id }).lean()
+  get: async function (id: Types.ObjectId): Promise<IGroup> {
+    try {
+      const res = await dbModel.findOne({ _id: id }).lean()
+      if (res === null) throw new NotFound('Group not found', 'The group you are trying to access does not exist')
+      return res
+    } catch (e) {
+      if (e instanceof NotFound) throw e
+      throw new DatabaseError('Failed to access data')
+    }
   },
   create: async function (data: IGroup): Promise<IGroup> {
+    validator.group.create(data)
     const created = await dbModel.create(data)
     return created.toObject()
+  },
+  update: async function (techLeadId: Types.ObjectId, groupId: Types.ObjectId, data: Partial<IGroup>): Promise<IGroup> {
+    validator.group.partial(data)
+    const isTechLead = await dbModel.exists({ _id: groupId, 'techLead._id': techLeadId })
+    if (isTechLead === null) throw new Forbidden('Access denied', 'Only tech leads can update group information')
+
+    const updated = await dbModel.updateOne({ _id: groupId }, data)
+    if (updated.matchedCount === 0) throw new NotFound('Group not found', 'The group you are trying to update does not exist')
+
+    const group = await dbModel.findOne({ _id: groupId }).lean()
+    if (group === null) throw new NotFound('Group not found', 'The group you are trying to update does not exist')
+
+    return group
   }
 }
 

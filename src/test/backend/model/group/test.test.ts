531f6ea04011dc1c7ae3239f64e32d09c150bf10
Jannael Orlando <orlandojannael@gmail.com>
2025-11-12 14:29:11 -0600

    exists group model fn test

--- src/test/backend/model/group/test.test.ts
+++ src/test/backend/model/group/test.test.ts
@@ -8,6 +8,7 @@ import dbModel from './../../../../backend/database/schemas/node/group'
 import { IEnv } from '../../../../backend/interface/env'
 import userDbModel from './../../../../backend/database/schemas/node/user'
 import { IGroup } from '../../../../backend/interface/group'
+import { Forbidden, NotFound } from '../../../../backend/error/error'
 
 dotenv.config({ quiet: true })
 const { DB_URL_ENV_TEST } = process.env as Pick<IEnv, 'DB_URL_ENV_TEST'>
@@ -47,14 +48,45 @@ describe('group model', () => {
       expect(res).toStrictEqual({
         name: 'test',
         color: '#000000',
-        _id: expect.any(Types.ObjectId),
-        techLead: [{ account: user.account, fullName: user.fullName }],
-        member: []
+        _id: expect.any(Types.ObjectId)
       })
     })
   })
 
-  describe('exists', () => {})
+  describe('exists', () => {
+    test('', async () => {
+      const res = await model.exists(group, user.account)
+      expect(res).toEqual(true)
+    })
+
+    test('error', async () => {
+      const cases = [
+        {
+          fn: async function () {
+            await model.exists(group, 'notExist@gmail.com')
+          },
+          error: new Forbidden('Access denied', 'The group exists but the user is not a techLead')
+        },
+        {
+          fn: async function () {
+            await model.exists({ ...group, _id: new mongoose.Types.ObjectId() }, user.account)
+          },
+          error: new NotFound('Group not found', 'The group you are trying to access does not exist')
+        }
+      ]
+
+      for (const { fn, error } of cases) {
+        try {
+          await fn()
+          throw new Error('Expected function to throw')
+        } catch (err: any) {
+          expect(err).toBeInstanceOf(error.constructor)
+          expect(err.message).toBe(error.message)
+          expect(err.description).toBe(error.description)
+        }
+      }
+    })
+  })
 
   describe('get', () => {})
 

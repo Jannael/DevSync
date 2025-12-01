5be0cb9dd03f4b5ce986b5f9bf6d734e65d8833e
Jannael Orlando <orlandojannael@gmail.com>
2025-12-01 16:39:33 -0600

    feat(controller): /controller/group/, get controller fn
    only executing the fn i've already created

--- src/backend/controller/group/controller.ts
+++ src/backend/controller/group/controller.ts
@@ -1,7 +1,17 @@
 import { Request, Response } from 'express'
+import fn from '../../function/group/function'
+import ErrorHandler from '../../error/handler'
+import { CustomError } from '../../error/error'
 
 const controller = {
-  get: async function (req: Request, res: Response) {},
+  get: async function (req: Request, res: Response) {
+    try {
+      const result = await fn.get(req, res)
+      res.json({ complete: true, result })
+    } catch (e) {
+      ErrorHandler.user(res, e as CustomError)
+    }
+  },
   create: async function (req: Request, res: Response) {},
   update: async function (req: Request, res: Response) {},
   delete: async function (req: Request, res: Response) {},

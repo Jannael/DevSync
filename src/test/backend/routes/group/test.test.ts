fd57b5aef31078b34925742c280fd6f9fbddace5
Jannael Orlando <orlandojannael@gmail.com>
2025-12-04 13:31:30 -0600

    test(setup group api): test/routes/group/, setting everything i need to test the enedpoint

---
+++ src/test/backend/routes/group/test.test.ts
@@ -0,0 +1,51 @@
+import { createApp } from '../../../../backend/app'
+import { Express } from 'express'
+import request from 'supertest'
+import mongoose from 'mongoose'
+import dotenv from 'dotenv'
+import { IEnv } from '../../../../backend/interface/env'
+import userModel from './../../../../backend/model/user/model'
+import userDbModel from './../../../../backend/database/schemas/node/user'
+import groupDbModel from './../../../../backend/database/schemas/node/group'
+import { IRefreshToken } from '../../../../backend/interface/user'
+
+dotenv.config({ quiet: true })
+const { DB_URL_ENV_TEST } = process.env as Pick<IEnv, 'DB_URL_ENV_TEST'>
+
+let app: Express
+beforeAll(async () => {
+  app = await createApp(DB_URL_ENV_TEST)
+})
+
+afterAll(async () => {
+  await groupDbModel.deleteMany({})
+  await userDbModel.deleteMany({})
+  await mongoose.connection.close()
+})
+
+describe('/group/v1/', () => {
+  describe('/get/', () => {
+    test('', async () => {})
+    test('error', async () => {})
+  })
+
+  describe('/create/', () => {
+    test('', async () => {})
+    test('error', async () => {})
+  })
+
+  describe('/update/', () => {
+    test('', async () => {})
+    test('error', async () => {})
+  })
+
+  describe('/member/update/role/', () => {
+    test('', async () => {})
+    test('error', async () => {})
+  })
+
+  describe('/member/remove/', () => {
+    test('', async () => {})
+    test('error', async () => {})
+  })
+})

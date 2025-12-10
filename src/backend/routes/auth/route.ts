73eaa188b06e95d9d5aa387286c06a7a1acf0fff
Jannael Orlando <orlandojannael@gmail.com>
2025-12-09 18:12:53 -0600

    ready to documentate group api

--- src/backend/routes/auth/route.ts
+++ src/backend/routes/auth/route.ts
@@ -3,19 +3,19 @@ import controller from './../../controller/auth/auth.controller'
 
 const router = Router()
 
-router.post('/request/code/', controller.request.code) // doc
-router.post('/verify/code/', controller.verify.code) // doc
+router.post('/request/code/', controller.request.code)
+router.post('/verify/code/', controller.verify.code)
 
-router.get('/request/accessToken/', controller.request.accessToken) // doc
-router.post('/request/refreshToken/code/', controller.request.refreshToken.code) // doc
-router.post('/request/refreshToken/', controller.request.refreshToken.confirm) // doc
+router.get('/request/accessToken/', controller.request.accessToken)
+router.post('/request/refreshToken/code/', controller.request.refreshToken.code)
+router.post('/request/refreshToken/', controller.request.refreshToken.confirm)
 
-router.patch('/account/request/code/', controller.account.request.code) // doc
-router.patch('/account/verify/code/', controller.account.verify.code) // doc
+router.patch('/account/request/code/', controller.account.request.code)
+router.patch('/account/verify/code/', controller.account.verify.code)
 
-router.patch('/password/request/code/', controller.pwd.request.code) // doc
-router.patch('/password/verify/code/', controller.pwd.verify.code) // doc
+router.patch('/password/request/code/', controller.pwd.request.code)
+router.patch('/password/verify/code/', controller.pwd.verify.code)
 
-router.post('/request/logout/', controller.request.logout) // doc
+router.post('/request/logout/', controller.request.logout)
 
 export default router

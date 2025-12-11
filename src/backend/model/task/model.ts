f0d1b251b35c286e8d5f84aa5907e4493724dbd2
Jannael Orlando <orlandojannael@gmail.com>
2025-12-10 19:19:59 -0600

    feat(model): /model/task/model.ts, list task model fn

--- src/backend/model/task/model.ts
+++ src/backend/model/task/model.ts
@@ -2,22 +2,37 @@ import { Types } from 'mongoose'
 import { CustomError, DatabaseError, NotFound } from '../../error/error'
 import handler from '../../error/handler'
 import dbModel from './../../database/schemas/node/task'
+import { IListTask, ITask } from '../../interface/task'
 
 const model = {
-  list: async function (groupId: Types.ObjectId, userAccount: string, pagination: number) {
+  list: async function (groupId: Types.ObjectId, userAccount: string, pagination: number): Promise<IListTask> {
     try {
-      const allTasks = await dbModel.find({ groupId }, { _id: 1, name: 1, priority: 1, isComplete: 1 }).skip(pagination * 10).limit(10).lean()
-      if (allTasks === null) throw new NotFound('Group not found', 'The group may not have any tasks')
-      // const response = allTasks.reduce((acc, task) => {
-      //   acc.task.push({
-      //     _id: task._id,
-      //     name: task.name,
-      //     priority: task.priority,
-      //     isComplete: task.isComplete
-      //   })
-      // }, { task: [], assign: [] })
+      const allTasks = await dbModel.find({ groupId }, {
+        _id: 1,
+        name: 1,
+        priority: 1,
+        isComplete: 1,
+        user: 1
+      }).skip(pagination * 10).limit(10).lean<
+      Array<Pick<ITask, '_id' | 'name' | 'priority' | 'isComplete' | 'user'>>
+      >()
 
-      return allTasks
+      if (allTasks === null) throw new NotFound('Group not found', 'The group may not have any tasks')
+
+      const response: IListTask = allTasks.reduce<IListTask>((acc, task) => {
+        acc.task.push({
+          _id: task._id,
+          name: task.name,
+          priority: task.priority,
+          isComplete: task.isComplete
+        })
+
+        if (task.user.includes(userAccount)) acc.assign.push(task._id)
+
+        return acc
+      }, { task: [], assign: [] })
+
+      return response
     } catch (e) {
       handler.allErrors(e as CustomError,
         new DatabaseError('Failed to access data', 'The task was not retrieved please try again')
@@ -25,9 +40,9 @@ const model = {
       throw new DatabaseError('Failed to access data', 'The task was not retrieved please try again')
     }
   },
-  get: async function (taskId: Types.ObjectId) {
+  get: async function (taskId: Types.ObjectId): Promise<ITask> {
     try {
-      const res = await dbModel.findOne({ _id: taskId })
+      const res = await dbModel.findOne({ _id: taskId }).lean<ITask>()
       if (res == null) throw new NotFound('Task not found')
       return res
     } catch (e) {

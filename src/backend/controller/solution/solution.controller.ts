ff68074577d625ae1b0330e4e713b6d4f28cde39
Jannael Orlando <orlandojannael@gmail.com>
2025-12-13 18:14:50 -0600

    chore(controller): controller/solution solution controller

--- src/backend/controller/solution/solution.controller.ts
+++ src/backend/controller/solution/solution.controller.ts
@@ -6,28 +6,32 @@ import { CustomError } from '../../error/error'
 const controller = {
   get: async function (req: Request, res: Response) {
     try {
-
+      const result = await service.get(req, res)
+      res.json({ complete: true, result })
     } catch (e) {
       handler.user(res, e as CustomError)
     }
   },
   create: async function (req: Request, res: Response) {
     try {
-
+      const result = await service.create(req, res)
+      res.json({ complete: true, result })
     } catch (e) {
       handler.user(res, e as CustomError)
     }
   },
   update: async function (req: Request, res: Response) {
     try {
-
+      const result = await service.update(req, res)
+      res.json({ complete: result })
     } catch (e) {
       handler.user(res, e as CustomError)
     }
   },
   delete: async function (req: Request, res: Response) {
     try {
-
+      const result = await service.delete(req, res)
+      res.json({ complete: result })
     } catch (e) {
       handler.user(res, e as CustomError)
     }
--- src/backend/routes/solution/route.ts
+++ src/backend/routes/solution/route.ts
@@ -4,10 +4,10 @@ import controller from './../../controller/solution/solution.controller'
 
 const router = Router()
 
-router.post('/get/', auth(['techLead', 'developer', 'documenter']))
+router.post('/get/', auth(['techLead', 'developer', 'documenter']), controller.get)
 
-router.put('/update/', auth(['techLead', 'developer']))
-router.post('/create/', auth(['techLead', 'developer']))
-router.delete('/delete/', auth(['techLead', 'developer']))
+router.put('/update/', auth(['techLead', 'developer']), controller.update)
+router.post('/create/', auth(['techLead', 'developer']), controller.create)
+router.delete('/delete/', auth(['techLead', 'developer']), controller.delete)
 
 export default router

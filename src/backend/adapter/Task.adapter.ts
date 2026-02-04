cafdcd0af1275e2c239696621be2f1d60d5524a4
Jannael Orlando <orlandojannael@gmail.com>
2026-02-04 12:16:42 -0600

    chore: TaskAdapter

--- src/backend/adapter/Task.adapter.ts
+++ src/backend/adapter/Task.adapter.ts
@@ -1,49 +1,12 @@
-import type { Request, Response } from 'express'
-import service from '../../controller/Task.controller'
-import type { CustomError } from '../../error/error'
-import handler from '../../error/handler'
+import TaskController from '../controller/Task.controller'
+import CreateAdapter from '../utils/helper/CreateAdapter.helper'
 
-const controller = {
-	list: async (req: Request, res: Response) => {
-		try {
-			const result = await service.list(req, res)
-			res.json({ success: true, result })
-		} catch (e) {
-			handler.user(res, e as CustomError)
-		}
-	},
-	get: async (req: Request, res: Response) => {
-		try {
-			const result = await service.get(req, res)
-			res.json({ success: true, result })
-		} catch (e) {
-			handler.user(res, e as CustomError)
-		}
-	},
-	create: async (req: Request, res: Response) => {
-		try {
-			const result = await service.create(req, res)
-			res.json({ success: true, result })
-		} catch (e) {
-			handler.user(res, e as CustomError)
-		}
-	},
-	update: async (req: Request, res: Response) => {
-		try {
-			const result = await service.update(req, res)
-			res.json({ success: result })
-		} catch (e) {
-			handler.user(res, e as CustomError)
-		}
-	},
-	delete: async (req: Request, res: Response) => {
-		try {
-			const result = await service.delete(req, res)
-			res.json({ success: result })
-		} catch (e) {
-			handler.user(res, e as CustomError)
-		}
-	},
+const TaskAdapter = {
+	Get: CreateAdapter({ controller: TaskController.Get }),
+	List: CreateAdapter({ controller: TaskController.List }),
+	Create: CreateAdapter({ controller: TaskController.Create }),
+	Update: CreateAdapter({ controller: TaskController.Update }),
+	Delete: CreateAdapter({ controller: TaskController.Delete }),
 }
 
-export default controller
+export default TaskAdapter
--- src/backend/route/Task.route.ts
+++ src/backend/route/Task.route.ts
@@ -1,15 +1,15 @@
 import { Router } from 'express'
-import Adapter from '../adapter/Task.adapter'
+import TaskAdapter from '../adapter/Task.adapter'
 import Roles, { ValidRoles } from '../constant/Role.constant'
 import RoleMiddleware from '../middleware/Role.middleware'
 
 const router = Router()
 
-router.post('/get/', RoleMiddleware(ValidRoles), Adapter.get)
-router.post('/list/', RoleMiddleware(ValidRoles), Adapter.list)
+router.post('/get/', RoleMiddleware(ValidRoles), TaskAdapter.Get)
+router.post('/list/', RoleMiddleware(ValidRoles), TaskAdapter.List)
 
-router.put('/update/', RoleMiddleware([Roles.techLead]), Adapter.update)
-router.post('/create/', RoleMiddleware([Roles.techLead]), Adapter.create)
-router.delete('/delete/', RoleMiddleware([Roles.techLead]), Adapter.delete)
+router.put('/update/', RoleMiddleware([Roles.techLead]), TaskAdapter.Update)
+router.post('/create/', RoleMiddleware([Roles.techLead]), TaskAdapter.Create)
+router.delete('/delete/', RoleMiddleware([Roles.techLead]), TaskAdapter.Delete)
 
 export default router

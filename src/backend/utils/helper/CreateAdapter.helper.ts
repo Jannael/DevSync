6de8b6e4b06487beeaed5b97cb1fc5e3a267d0e7
Jannael Orlando <orlandojannael@gmail.com>
2026-02-05 21:57:44 -0600

    chore: adding transctions at CreateAdapter

--- src/backend/utils/helper/CreateAdapter.helper.ts
+++ src/backend/utils/helper/CreateAdapter.helper.ts
@@ -1,17 +1,31 @@
 import type { Request, Response } from 'express'
+import mongoose from 'mongoose'
 import type { CustomError } from '../../error/Error.constructor'
 import ErrorHandler from '../../error/Error.handler'
 
 function CreateAdapter({
 	controller,
 	link,
+	options,
 }: {
 	controller: (req: Request, res: Response) => unknown
 	link?: Array<{ rel: string; href: string }>
+	options?: { useTransaction?: boolean }
 }) {
 	return async (req: Request, res: Response) => {
+		const { useTransaction } = options ?? {}
+		const session = useTransaction ? await mongoose.startSession() : undefined
+
 		try {
-			const result = await controller(req, res)
+			let result: unknown
+			if (useTransaction) {
+				await session?.withTransaction(async () => {
+					result = await controller(req, res)
+				})
+			} else {
+				result = await controller(req, res)
+			}
+
 			if (typeof result === 'boolean') {
 				res.json({ success: result })
 			} else {
@@ -21,6 +35,8 @@ function CreateAdapter({
 			;(e as CustomError).link = link
 
 			ErrorHandler.Response({ res, error: e as CustomError })
+		} finally {
+			if (useTransaction) await session?.endSession()
 		}
 	}
 }

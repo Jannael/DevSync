923f8d574de7d5f866511c3aa0adf1403bfde907
Jannael Orlando <orlandojannael@gmail.com>
2026-02-05 22:03:56 -0600

    i regret to add mongoose transctions let first check them out and then ill implement them

--- src/backend/utils/helper/CreateAdapter.helper.ts
+++ src/backend/utils/helper/CreateAdapter.helper.ts
@@ -1,31 +1,17 @@
 import type { Request, Response } from 'express'
-import mongoose from 'mongoose'
 import type { CustomError } from '../../error/Error.constructor'
 import ErrorHandler from '../../error/Error.handler'
 
 function CreateAdapter({
 	controller,
 	link,
-	options,
 }: {
 	controller: (req: Request, res: Response) => unknown
 	link?: Array<{ rel: string; href: string }>
-	options?: { useTransaction?: boolean }
 }) {
 	return async (req: Request, res: Response) => {
-		const { useTransaction } = options ?? {}
-		const session = useTransaction ? await mongoose.startSession() : undefined
-
 		try {
-			let result: unknown
-			if (useTransaction) {
-				await session?.withTransaction(async () => {
-					result = await controller(req, res)
-				})
-			} else {
-				result = await controller(req, res)
-			}
-
+			const result = await controller(req, res)
 			if (typeof result === 'boolean') {
 				res.json({ success: result })
 			} else {
@@ -35,8 +21,6 @@ function CreateAdapter({
 			;(e as CustomError).link = link
 
 			ErrorHandler.Response({ res, error: e as CustomError })
-		} finally {
-			if (useTransaction) await session?.endSession()
 		}
 	}
 }

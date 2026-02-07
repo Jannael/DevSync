23188f621701a4b5bf41f4e681dc610fe2e192a8
Jannael Orlando <orlandojannael@gmail.com>
2026-02-07 12:27:17 -0600

    chore: HATEOAS for task.adapter.ts

--- src/backend/adapter/Task.adapter.ts
+++ src/backend/adapter/Task.adapter.ts
@@ -2,11 +2,51 @@ import TaskController from '../controller/Task.controller'
 import CreateAdapter from '../utils/helper/CreateAdapter.helper'
 
 const TaskAdapter = {
-	Get: CreateAdapter({ controller: TaskController.Get }),
-	List: CreateAdapter({ controller: TaskController.List, options: { transaction: true } }),
-	Create: CreateAdapter({ controller: TaskController.Create }),
-	Update: CreateAdapter({ controller: TaskController.Update }),
-	Delete: CreateAdapter({ controller: TaskController.Delete, options: { transaction: true } }),
+	Get: CreateAdapter({
+		controller: TaskController.Get,
+		SuccessLink: [
+			{ rel: 'self', href: '/task/v1/get/' },
+			{ rel: 'list', href: '/task/v1/list/' },
+			{ rel: 'update', href: '/task/v1/update/' },
+			{ rel: 'delete', href: '/task/v1/delete/' },
+			{ rel: 'solution', href: '/solution/v1/get/' },
+		],
+	}),
+	List: CreateAdapter({
+		controller: TaskController.List,
+		options: { transaction: true },
+		SuccessLink: [
+			{ rel: 'self', href: '/task/v1/list/' },
+			{ rel: 'get', href: '/task/v1/get/' },
+			{ rel: 'create', href: '/task/v1/create/' },
+		],
+	}),
+	Create: CreateAdapter({
+		controller: TaskController.Create,
+		SuccessLink: [
+			{ rel: 'self', href: '/task/v1/create/' },
+			{ rel: 'get', href: '/task/v1/get/' },
+			{ rel: 'list', href: '/task/v1/list/' },
+		],
+	}),
+	Update: CreateAdapter({
+		controller: TaskController.Update,
+		SuccessLink: [
+			{ rel: 'self', href: '/task/v1/update/' },
+			{ rel: 'get', href: '/task/v1/get/' },
+			{ rel: 'list', href: '/task/v1/list/' },
+			{ rel: 'delete', href: '/task/v1/delete/' },
+		],
+	}),
+	Delete: CreateAdapter({
+		controller: TaskController.Delete,
+		options: { transaction: true },
+		SuccessLink: [
+			{ rel: 'self', href: '/task/v1/delete/' },
+			{ rel: 'list', href: '/task/v1/list/' },
+			{ rel: 'create', href: '/task/v1/create/' },
+		],
+	}),
 }
 
 export default TaskAdapter

55c65e15e6dab66c18c741cacb4fe99c974ee361
Jannael Orlando <orlandojannael@gmail.com>
2026-02-07 18:12:48 -0600

    chore: HATEOS

--- src/backend/adapter/Auth.adapter.ts
+++ src/backend/adapter/Auth.adapter.ts
@@ -8,6 +8,7 @@ const AuthAdapter = {
 			{ rel: 'self', href: '/auth/v1/request/code/' },
 			{ rel: 'verify', href: '/auth/v1/verify/code/' },
 		],
+		ErrorLink: [{ rel: 'self', href: '/auth/v1/request/code/' }],
 	}),
 	VerifyCode: CreateAdapter({
 		controller: AuthController.VerifyCode,
@@ -17,12 +18,20 @@ const AuthAdapter = {
 			{ rel: 'update', href: '/user/v1/update/' },
 			{ rel: 'delete', href: '/user/v1/delete/' },
 		],
+		ErrorLink: [
+			{ rel: 'self', href: '/auth/v1/verify/code/' },
+			{ rel: 'code', href: '/auth/v1/request/code/' },
+		],
 	}),
 	RequestAccessToken: CreateAdapter({
 		controller: AuthController.Request.AccessToken,
 		SuccessLink: [
 			{ rel: 'self', href: '/auth/v1/request/accessToken/' },
 			{ rel: 'user', href: '/user/v1/get/' },
+		],
+		ErrorLink: [
+			{ rel: 'self', href: '/auth/v1/request/accessToken/' },
+			{ rel: 'login', href: '/auth/v1/request/refreshToken/code/' },
 		],
 	}),
 	RequestRefreshTokenCode: CreateAdapter({
@@ -31,6 +40,7 @@ const AuthAdapter = {
 			{ rel: 'self', href: '/auth/v1/request/refreshToken/code/' },
 			{ rel: 'token', href: '/auth/v1/request/refreshToken/' },
 		],
+		ErrorLink: [{ rel: 'self', href: '/auth/v1/request/refreshToken/code/' }],
 	}),
 	RequestRefreshToken: CreateAdapter({
 		controller: AuthController.RefreshToken.Confirm,
@@ -38,6 +48,7 @@ const AuthAdapter = {
 			{ rel: 'self', href: '/auth/v1/request/refreshToken/' },
 			{ rel: 'accessToken', href: '/auth/v1/request/accessToken/' },
 		],
+		ErrorLink: [{ rel: 'self', href: '/auth/v1/request/refreshToken/' }],
 	}),
 	AccountRequestCode: CreateAdapter({
 		controller: AuthController.Account.RequestCode,
@@ -45,6 +56,7 @@ const AuthAdapter = {
 			{ rel: 'self', href: '/auth/v1/account/request/code/' },
 			{ rel: 'change', href: '/auth/v1/change/account/' },
 		],
+		ErrorLink: [{ rel: 'self', href: '/auth/v1/account/request/code/' }],
 	}),
 	ChangeAccount: CreateAdapter({
 		controller: AuthController.Account.Change,
@@ -53,6 +65,10 @@ const AuthAdapter = {
 			{ rel: 'self', href: '/auth/v1/change/account/' },
 			{ rel: 'user', href: '/user/v1/get/' },
 		],
+		ErrorLink: [
+			{ rel: 'self', href: '/auth/v1/change/account/' },
+			{ rel: 'code', href: '/auth/v1/account/request/code/' },
+		],
 	}),
 	PasswordRequestCode: CreateAdapter({
 		controller: AuthController.Pwd.RequestCode,
@@ -60,6 +76,7 @@ const AuthAdapter = {
 			{ rel: 'self', href: '/auth/v1/password/request/code/' },
 			{ rel: 'change', href: '/auth/v1/change/password/' },
 		],
+		ErrorLink: [{ rel: 'self', href: '/auth/v1/password/request/code/' }],
 	}),
 	ChangePassword: CreateAdapter({
 		controller: AuthController.Pwd.Change,
@@ -67,10 +84,18 @@ const AuthAdapter = {
 			{ rel: 'self', href: '/auth/v1/change/password/' },
 			{ rel: 'logout', href: '/auth/v1/request/logout/' },
 		],
+		ErrorLink: [
+			{ rel: 'self', href: '/auth/v1/change/password/' },
+			{ rel: 'code', href: '/auth/v1/password/request/code/' },
+		],
 	}),
 	Logout: CreateAdapter({
 		controller: AuthController.Request.Logout,
 		SuccessLink: [
+			{ rel: 'self', href: '/auth/v1/request/logout/' },
+			{ rel: 'login', href: '/auth/v1/request/refreshToken/code/' },
+		],
+		ErrorLink: [
 			{ rel: 'self', href: '/auth/v1/request/logout/' },
 			{ rel: 'login', href: '/auth/v1/request/refreshToken/code/' },
 		],

5cde0dab6efb6a4fc87ac10eb3bf847253a8e8a2
Jannael Orlando <orlandojannael@gmail.com>
2026-04-26 11:34:25 -0600

    feat: copy template fn

--- apps/cli/modules/build/infra/build-repository.ts
+++ apps/cli/modules/build/infra/build-repository.ts
@@ -1,19 +1,36 @@
-import type { BuildRepository } from '../domain/build-repository'
+import type { BuildRepository } from '@/modules/build/domain/build-repository'
+import { cp, readdir } from 'node:fs/promises'
+import { join, resolve } from 'node:path'
+
+const TEMPLATE_DIRECTORY = resolve(import.meta.dir, '..', '..', '..', '..', 'template')
 
 class BuildRepositoryImpl implements BuildRepository {
-  async copyTemplate(): Promise<void> {}
+  async copyTemplate(): Promise<void> {
+    const entries = await readdir(TEMPLATE_DIRECTORY, { withFileTypes: true })
 
-  async readFile({ path }: { path: string }): Promise<string> {
+    for (const entry of entries) {
+      const sourcePath = join(TEMPLATE_DIRECTORY, entry.name)
+      const destinationPath = join(process.cwd(), entry.name)
+
+      await cp(sourcePath, destinationPath, {
+        recursive: true,
+        force: false,
+        errorOnExist: true,
+      })
+    }
+  }
+
+  async readFile({ path: _path }: { path: string }): Promise<string> {
     return ''
   }
 
-  async getHTMLFromComponent({ component }: { component: string }): Promise<string> {
+  async getHTMLFromComponent({ component: _component }: { component: string }): Promise<string> {
     return ''
   }
 
-  async createPDF({ html, path }: { html: string; path: string }): Promise<void> {}
+  async createPDF({ html: _html, path: _path }: { html: string; path: string }): Promise<void> {}
 
-  async writeFile({ path, data }: { path: string; data: string }): Promise<void> {}
+  async writeFile({ path: _path, data: _data }: { path: string; data: string }): Promise<void> {}
 }
 
 export default BuildRepositoryImpl

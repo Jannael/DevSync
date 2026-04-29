800103b34eacef3cc01c3f381826fb1f52cae726
Jannael Orlando <orlandojannael@gmail.com>
2026-04-28 20:11:41 -0600

    fix: ref to sitemap.xml

--- apps/template/src/pages/robots.txt.ts
+++ apps/template/src/pages/robots.txt.ts
@@ -8,7 +8,7 @@ export const GET: APIRoute = async ({ site }) => {
 User-agent: *
 Allow: /
 
-Sitemap: ${new URL('sitemap-index.xml', siteUrl)}
+Sitemap: ${new URL('sitemap.xml', siteUrl)}
   `.trim(), {
     headers: {
       'Content-Type': 'text/plain; charset=utf-8',

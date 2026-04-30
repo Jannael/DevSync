f2901707912255d857acf67ba78472199df4c321
Jannael Orlando <orlandojannael@gmail.com>
2026-04-30 12:11:26 -0600

    fix: global devsync fields where going as languages to astro

--- apps/template/src/devsync.ts
+++ apps/template/src/devsync.ts
@@ -3,7 +3,18 @@ import { parseDevsync } from './devsync-validator'
 
 export * from './devsync-validator'
 
-export const languages = Object.keys(devsync).filter((key) => key !== 'defaultLang')
+const devsyncGlobalFields = [
+  'name',
+  'img',
+  'socialMedia',
+  'site',
+  'githubUserName',
+  'defaultLang',
+] as const
+
+export const languages = Object.keys(devsync).filter(
+  (key) => !devsyncGlobalFields.includes(key as (typeof devsyncGlobalFields)[number])
+)
 export const defaultLang = devsync.defaultLang
 
 export default parseDevsync(devsync)

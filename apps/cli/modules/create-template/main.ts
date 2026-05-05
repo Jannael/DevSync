07c57c41a2488ce8a513de2e46e558c95dda1ea3
Jannael Orlando <orlandojannael@gmail.com>
2026-05-05 08:47:23 -0600

    feat: ascii art add create-template command

--- apps/cli/modules/create-template/main.ts
+++ apps/cli/modules/create-template/main.ts
@@ -1,8 +1,10 @@
 import { copyDirectory } from '@/shared/infra/copy-directory'
 import { DEVSYNC_DIRECTORY } from '@/constants/paths'
 import { CHECK, SPACE } from '@/utils/icons-terminal'
+import PrintASCII from '@/ascii'
 
 export default async function createTemplate() {
+  PrintASCII()
   console.log(`${SPACE}Creating template...`)
   await copyDirectory(DEVSYNC_DIRECTORY, process.cwd())
   console.log(`${SPACE}${CHECK('Template created successfully!')}`)

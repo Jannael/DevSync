043ac86af1b4070da94b60e694aad9ce1cbbfa84
Jannael Orlando <orlandojannael@gmail.com>
2026-05-14 13:21:50 -0600

    more docs

--- apps/web/astro.config.mjs
+++ apps/web/astro.config.mjs
@@ -8,6 +8,49 @@ export default defineConfig({
     starlight({
       title: 'Devsync documentation',
       social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/jannael/devsync' }],
+      sidebar: [
+        {
+          label: 'Getting Started',
+          link: '/',
+        },
+        {
+          label: 'Commands',
+          link: '/commands/',
+        },
+        {
+          label: 'Configuration',
+          link: '/configuration/',
+        },
+        {
+          label: 'Create Custom Template',
+          link: '/create-template/',
+        },
+        {
+          label: 'GitHub Actions',
+          link: '/github-actions/',
+        },
+        {
+          label: 'How It Works',
+          items: [
+            {
+              label: 'Init Command',
+              link: '/how-it-works/init-command/',
+            },
+            {
+              label: 'Build Command',
+              link: '/how-it-works/build-command/',
+            },
+            {
+              label: 'Create Template',
+              link: '/how-it-works/create-template/',
+            },
+          ],
+        },
+        {
+          label: 'Troubleshooting',
+          link: '/troubleshooting/',
+        },
+      ],
     }),
   ],
 })

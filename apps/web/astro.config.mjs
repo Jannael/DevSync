22f358b67d4246df1836310ac570ab804efe9e2c
Jannael Orlando <orlandojannael@gmail.com>
2026-05-14 10:00:44 -0600

    docs: start docs

---
+++ apps/web/astro.config.mjs
@@ -0,0 +1,13 @@
+// @ts-check
+import { defineConfig } from 'astro/config'
+import starlight from '@astrojs/starlight'
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [
+    starlight({
+      title: 'Devsync documentation',
+      social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/jannael/devsync' }],
+    }),
+  ],
+})

d270070c16f4ada9897fde24da40dc7b65ff7a70
Jannael Orlando <orlandojannael@gmail.com>
2026-05-15 15:53:50 -0600

    utility for translations

---
+++ apps/web/src/i18n/translation.ts
@@ -0,0 +1,13 @@
+import en from './en.json'
+import es from './es.json'
+
+export const translations = {
+  en,
+  es,
+} as const
+
+export type Language = keyof typeof translations
+
+export function getTranslation(lang: string) {
+  return translations[lang as Language] || translations.en
+}

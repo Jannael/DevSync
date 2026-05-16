4ac6f134a72e8a1517b34157d24c3fed969cb604
Jannael Orlando <orlandojannael@gmail.com>
2026-05-16 12:16:52 -0600

    chore: robots.txt

---
+++ apps/web/src/pages/robots.txt.ts
@@ -0,0 +1,15 @@
+export function GET() {
+  const sitemapUrl = 'https://devsync.work/sitemap-index.xml'
+
+  const robotsTxt = `User-agent: *
+Allow: /
+
+Sitemap: ${sitemapUrl}
+`
+
+  return new Response(robotsTxt, {
+    headers: {
+      'Content-Type': 'text/plain',
+    },
+  })
+}
